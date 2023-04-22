a281f045f37c9b7acc360656d2b02546c85651c4
NandiNotha <129956886+NandiNotha@users.noreply.github.com>
2023-04-22 22:00:26 +0200

    changes

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +12,23 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-   heroes = HEROES;
+  selectedHero?: Hero;
+  
+  heroes: Hero[] = [];
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
    
-   selectedHero?: Hero;
-   onSelect(hero: Hero): void {
-     this.selectedHero = hero;
-   }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
